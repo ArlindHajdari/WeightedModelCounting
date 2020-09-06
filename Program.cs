9a5f34918b6d3d57def88f0dee9e9e0a6c56b0ac
Arlind Hajdari <arlind.hajdari@gmail.com>
2020-09-06 02:10:43 +0200

    Initial commit
    Read and print the solution candidate

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WeightedModelCounting.Extensions;
+using WeightedModelCounting.Models;
+
+namespace WeightedModelCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Instance instance = IO.ReadCNF("Inputs/track2_000.wcnf");
+            Console.WriteLine(instance.ToString());
+            Console.ReadKey();
+        }
+    }
+}
